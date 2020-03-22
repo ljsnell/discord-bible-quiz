1181cb3a2a585dd257e51b220209d3a8ee28d8fe
ljsnell <ljsnell7@gmail.com>
2020-03-22 12:29:05 -0500

    Updated to account for all the part types.

--- bot.js
+++ bot.js
@@ -9,17 +9,26 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+//  TODO: Convert to case statement
 client.on('message', msg => {
     if (msg.content === '!ref') {
         readQuestionFile(msg, './questions/ref.json');
     } else if (msg.content === '!general') {
         readQuestionFile(msg, './questions/general.json');
-    } else if (msg.content === '!part') {
-        readQuestionFile(msg, './questions/part.json');
+    } else if (msg.content === '!2part') {
+        readQuestionFile(msg, './questions/2part.json');
+    } else if (msg.content === '!3part') {
+        readQuestionFile(msg, './questions/3part.json');
+    } else if (msg.content === '!4part') {
+        readQuestionFile(msg, './questions/4part.json');
+    } else if (msg.content === '!5part') {
+        readQuestionFile(msg, './questions/5part.json');
+    } else if (msg.content === '!multiple') {
+        readQuestionFile(msg, './questions/multiple.json');
     } else if (msg.content === '!ftv') {
         readQuestionFile(msg, './questions/ftv.json');
     } else if (msg.content === '!help') {
-        msg.reply("Supported commands are !ref, !general, !part, and !ftv")
+        msg.reply("Supported commands are !ref, !general, !2part, !3part, !4part, !5part, !multiple, and !ftv")
     }
 });
 
