e9f94255efd380ac96e4145b73371586353b4251
ljsnell <ljsnell7@gmail.com>
2020-03-21 22:01:20 -0500

    Updated code to not spam the messages.

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
+const fs = require('fs');
+const discordSpoilerTag = "||"
+const space = " "
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -9,14 +12,20 @@ client.on('ready', () => {
 client.on('message', msg => {
     if (msg.content === '!ref') {
         rawdata = fs.readFileSync('./questions/ref.json');
+        // Setup the question response
+        let questionList = JSON.parse(rawdata);
+        var randomQuestion = questionList[Math.floor(Math.random()*questionList.length)];
+        // Build actual response   
+        msg.reply(JSON.stringify(randomQuestion["Q"] + space + discordSpoilerTag 
+            + randomQuestion["A"] + discordSpoilerTag));
     } else if (msg.content === '!general') {
         rawdata = fs.readFileSync('./questions/general.json');
     } else if (msg.content === '!part') {
         rawdata = fs.readFileSync('./questions/part.json');
     } else if (msg.content === '!ftv') {
         rawdata = fs.readFileSync('./questions/ftv.json');
-    } else {
-        rawdata = fs.readFileSync('./questions/not_found.json');
+    } else if (msg.content === '!help') {
+        msg.reply("Supported commands are !ref, !general, !part, and !ftv")
     }
 });
 
