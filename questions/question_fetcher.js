4d47fafc1c1c322c17da8593bfe6031e5fb07207
ljsnell <ljsnell7@gmail.com>
2020-03-21 20:49:42 -0500

    Added spoiler tags around answer

--- questions/question_fetcher.js
+++ questions/question_fetcher.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const discordSpoilerTag = "||"
 
 const server = http.createServer((req, res) => {
   let rawdata = fs.readFileSync('Refs.json');
@@ -13,7 +14,8 @@ const server = http.createServer((req, res) => {
   var randomQuestion = questionList[Math.floor(Math.random()*questionList.length)];
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(randomQuestion));
+  res.end(JSON.stringify(randomQuestion["Q"] + discordSpoilerTag 
+    + randomQuestion["A"] + discordSpoilerTag));
 });
 
 server.listen(port, hostname, () => {
