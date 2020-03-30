3a4b19c4143f2c78cdccc63811c7f35a0fbd2eae
ljsnell <ljsnell7@gmail.com>
2020-03-29 19:49:57 -0500

    Simplified to make it use parameters. Might work?

--- botv2.js
+++ botv2.js
@@ -9,37 +9,16 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-//  TODO: Convert to case statement
+
 client.on('message', msg => {
-    
-    if (msg.content.startsWith('!general')) {
-        questionType = 1
-        queryDB();
-    } else if (msg.content.startsWith('!2part')) {
-        questionType = 2
-        queryDB();
-    } else if (msg.content.startsWith('!3part')) {
-        questionType = 3
-        queryDB();
-    } else if (msg.content.startsWith('!4part')) {
-        questionType = 4
-        queryDB();
-    } else if (msg.content.startsWith('!5part')) {
-        questionType = 5
-        queryDB();
-    } else if (msg.content.startsWith('!multiple')) {
-        questionType = 6
-        queryDB();
-    } else if (msg.content.startsWith('!ftv')) {
-        questionType = 7
-        queryDB();
-    } else if (msg.content.startsWith('!ref')) {
-        questionType = 8
-        queryDB();
-    } else if (msg.content.startsWith('!situation')) {
-        questionType = 9
-        queryDB();
-    }else if (msg.content.startsWith('!help')) {
+    // Example call: !quiz 1:1:1,2,3
+    // Selects a general question from the first 3 chapters
+    if (msg.content.startsWith('!quiz')) {
+        const args = message.content.slice(5).split(':');
+        const command = args.shift().toLowerCase();
+        msg.reply(queryDB(args[0], args[1], args[2]))
+    }
+    else if (msg.content.startsWith('!help')) {
         msg.reply("Supported commands are !ref, !general, !2part, !3part, !4part, !5part, !multiple, !ftv, and !situation")
     }
     msg.reply('sup');
@@ -48,10 +27,10 @@ client.on('message', msg => {
 client.login(auth.token);
 
 async function queryDB(questionType, bookNumber) {
-    // Example Query:
-    // select * from Questions where QType = 8 AND 
-    // Quality = 'a' and Book = 3 ORDER by RANDOM() LIMIT 1
+    // Example Query:    
+    // select * from Questions where QType = 1 AND Quality = 'a' 
+    //      and Book = 3 and Chapter in (1,2) ORDER by RANDOM() LIMIT 1
     // https://discordjs.guide/creating-your-bot/commands-with-user-input.html#basic-arguments
-    query = 'SELECT * from Questions where QType = ' + questionType + ' Quality = "a" and Book = ' 
-        + bookNumber + ' ORDER by RANDOM() LIMIT 1'
+    query = 'SELECT * from Questions where QType = ' + questionType + ' Quality = "a" and Book in(' 
+        + bookNumber + ') and Chapter in(' + chapter +') ORDER by RANDOM() LIMIT 1'
 }
